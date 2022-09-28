44689d4069056e7859204c80c38db98a026d869e
viaroska <92136376+viaroska@users.noreply.github.com>
2022-09-28 19:56:44 -0300

    1
    COMMIT

---
+++ Assets/sc/playerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float horizontal;
+    public float vertical;
+    public float Velocidad;
+
+    public CharacterController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        player.Move(new Vector3(horizontal , 0 , vertical)*Velocidad*Time.deltaTime);
+    }
+}
